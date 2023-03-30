e44910f0a8961889c9de17acc423ed2a2dee615e
hicode-dev <kingdavies2021@gmail.com>
2023-03-30 16:01:49 -0700

    i added more to the navbar

---
+++ src/component/CarouselPage.jsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import {FaSearch}  from 'react-icons/fa'
+const CarouselPage = () => {
+  return (
+    <div>
+       <header>
+    <div class="empty">
+      <div class="top-navigation">
+      <nav> 
+      
+  
+            <form class="search shadow-lg "> 
+              <div class="search-inputs">
+                <label for="location"> 
+                  <div class="label">Location</div>
+                  <input name="location" id="location" placeholder="Where are you going?"/>
+                </label>
+                
+              </div>
+              <div class="search-inputs">
+                <label for="check-in"> 
+                  <div class="label">Check in</div>
+                  <input name="check-in" id="check-in" placeholder="Add dates"/>
+                </label>
+              </div>
+              <div class="search-inputs">
+                <label for="check-out"> 
+                  <div class="label">Check out</div>
+                  <input name="check-out" id="check-out" placeholder="Add dates"/>
+                </label>
+              </div>
+              <div class="search-inputs">
+                <label for="guests"> 
+                  <div class="label">Guests</div>
+                  <input name="guests" id="guests" placeholder="Add dates"/>
+                </label>
+              </div>
+              <div class="search-button">
+                <button onclick="preventDefault()"><FaSearch /></button>
+              </div>
+            </form>
+        </nav> 
+  
+      
+  
+  
+      </div> 
+    </div>  
+  </header>
+    </div>
+  )
+}
+
+export default CarouselPage
--- src/component/Nav.jsx
+++ src/component/Nav.jsx
@@ -1,32 +1,51 @@
-import React from 'react'
+import React, { useState } from 'react'
 import image1 from '../assets/images-removebg-preview.png'
-import {FaSearch,FaUserCircle} from 'react-icons/fa'
-import {TbWorld} from 'react-icons/tb'
-import {FiMenu} from 'react-icons/fi'
+import { FaSearch, FaUserCircle } from 'react-icons/fa'
+import { TbWorld } from 'react-icons/tb'
+import { FiMenu } from 'react-icons/fi'
+import CarouselPage from './CarouselPage'
 const Nav = () => {
+  const [hide, setHide] = useState(false)
+  const HandleSubmit = () => {
+    setHide(!hide)
+    console.log(hide);
+  }
   return (
-    <div className='bg-slate-50 flex px-9 justify-between border-2 items-center shadow-lg
+<div className='bg-slate-50  shadow-lg  border-2 py-5'>
+<div className='flex px-9 justify-between items-center
     '>
       <div className='hidden md:block'>
-        <img src={image1} className="w-44"   />
+        <img src={image1} className="w-44" />
+      </div>
+      <div>
+        <div onClick={HandleSubmit}>
+          <div className="mysearch border-2 rounded-full flex items-center py-3 px-4 shadow-lg">
+            <button className="anywhere">Anywhere</button>
+            <button className="anyweek">Any week</button>
+            <button className="addguest">Add guests</button>
+            <div className="search-case bg-red-500 rounded-full p-3">
+              < FaSearch className='bg-red-500  text-white rounded-full ' />
+            </div>
+          </div>
         </div>
-          <div>
-             <div className="search bg-slate-200 px-5 flex items-center gap-4 rounded-full border">
-              <input type="text" className='bg-slate-200  outline-none py-3 rounded-full w-[220px] md:w-[400px]' />
-              <FaSearch/>
-             </div>
 
-          </div>
-
-          <div className="switch-to-hosting flex gap-3 items-center">
-            <h3>Switch to hosting</h3>
-              <TbWorld size={25} />
-              <div className='flex p-2 items-center rounded-full gap-2 bg-slate-200'>
-                <FiMenu size={25} />
-                <FaUserCircle size={25} />
-              </div>
-          </div>
+      </div>
+    
+      <div className="switch-to-hosting flex gap-3 items-center">
+        <h3>Switch to hosting</h3>
+        <TbWorld size={25} />
+        <div className='flex p-2 items-center rounded-full gap-2 bg-slate-200'>
+          <FiMenu size={25} />
+          <FaUserCircle size={25} />
+        </div>
+      </div>
+      
     </div>
+    <div className='flex justify-center'>
+                <CarouselPage />
+
+    </div>
+</div>
   )
 }
 
