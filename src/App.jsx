0b44078cbbf48c74932c9efc2733e1a0d3b1d521
hicode-dev <kingdavies2021@gmail.com>
2023-03-30 17:18:59 -0700

    jsadhnuj

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Nav from './component/Nav'
+import Tabs from './component/Tabs'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,6 +12,7 @@ function App() {
     <main>
 
     <Nav />
+    <Tabs />
     </main>
   )
 }
